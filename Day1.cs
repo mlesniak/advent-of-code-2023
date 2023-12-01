2ed0ecf4c0ce85e22100ac60fe9f3151f66ad2bf
Michael Lesniak <mail@mlesniak.com>
2023-12-01 13:24:37 +0100

    Part 2 example

--- Day1.cs
+++ Day1.cs
@@ -22,4 +22,57 @@ public class Day1
             .Sum();
         Console.WriteLine(result);
     }
+
+    public static void Part2()
+    {
+        var mapping = new Dictionary<string, int>
+        {
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9}
+        };
+
+        var result = File
+            .ReadAllLines("1.txt")
+            .Select(line =>
+            {
+                List<int> digits = new();
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (char.IsDigit(line[i]))
+                    {
+                        var num = line[i] - '0';
+                        digits.Add(num);
+                        continue;
+                    }
+
+                    foreach (KeyValuePair<string, int> pair in mapping)
+                    {
+                        if (line.Substring(i).StartsWith(pair.Key))
+                        {
+                            digits.Add(pair.Value);
+                            break;
+                        }
+                    }
+                }
+
+                return digits;
+            })
+            // .Select(k =>
+            // {
+            //     Console.WriteLine(string.Join(' ', k));
+            //     return k;
+            // })
+            .Select(ints => ints[0] * 10 + ints[^1])
+            .Sum();
+        Console.WriteLine(result);
+    }
+
 }
