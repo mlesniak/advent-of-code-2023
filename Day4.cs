66ffb17fb870363107cd56d93ad286373d8d9e3a
Michael Lesniak <mail@mlesniak.com>
2023-12-04 09:46:29 +0100

    Part 2

--- Day4.cs
+++ Day4.cs
@@ -40,20 +40,26 @@ public class Day4
     {
         var lines = File.ReadAllLines("4.txt");
 
+        var cache = new Dictionary<string, (int, long)>();
+
         var q = new Queue<string>();
         foreach (var line in lines)
         {
             q.Enqueue(line);
         }
 
+        var counts = new Dictionary<int, int>();
         long result = 0;
         while (q.Any())
         {
             var line = q.Dequeue();
             result++;
-            (int cardNo, long correctNumbers) = ComputeCorrectNumbers(line);
+            (int cardNo, long correctNumbers) = ComputeCorrectNumbers(cache, line);
+
+            counts[cardNo] = counts.GetValueOrDefault(cardNo, 0) + 1;
+
             // Console.WriteLine($"{cardNo}: {correctNumbers}");
-            for (int i = cardNo+1; i < lines.Length && i < cardNo + 1 + correctNumbers; i++)
+            for (int i = cardNo+1; i <= lines.Length && i < cardNo + 1+ correctNumbers; i++)
             {
                 // Console.WriteLine($"  {i}: {lines[i]}");
                 q.Enqueue(lines[i-1]);
@@ -61,12 +67,25 @@ public class Day4
         }
 
         Console.WriteLine(result);
+        // foreach (var keyValuePair in counts)
+        // {
+            // Console.WriteLine(keyValuePair);
+        // }
+        // foreach (var keyValuePair in cache)
+        // {
+        //     Console.WriteLine(keyValuePair);
+        // }
     }
 
-    private static (int, long) ComputeCorrectNumbers(string line)
+    private static (int, long) ComputeCorrectNumbers(Dictionary<string, (int, long)> cache, string line)
     {
+        if (cache.TryGetValue(line, out (int, long) result))
+        {
+            return result;
+        }
+
         var nums = line.Split(":")[1];
-        var cardNo = Int32.Parse(line.Split(":")[0].Split(" ")[1]);
+        var cardNo = Int32.Parse(line.Split(":")[0].Split(" ")[^1].Trim());
 
         var parts = nums.Split("|");
         var winning = parts[0]
@@ -83,7 +102,9 @@ public class Day4
             .ToHashSet();
 
         chosen.IntersectWith(winning);
-        return (cardNo, chosen.Count);
+        var computed = (cardNo, chosen.Count);
+        cache[line] = computed;
+        return computed;
     }
 
 }
