2e2ee6143c8ef4f1b2289c0f9b5412d5264a5a24
Michael Lesniak <mail@mlesniak.com>
2023-12-27 20:04:56 +0100

    Read file

--- Day16.cs
+++ Day16.cs
@@ -4,10 +4,26 @@ public class Day16
 {
     public static void Part1()
     {
+        var mirrors = new Dictionary<Position, char>();
         var lines = File.ReadAllLines("16.txt");
-        foreach (var line in lines)
+        for (var y = 0; y < lines.Length; y++)
         {
-            Console.WriteLine(line);
+            var line = lines[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                var c = line[x];
+                if (c == '.')
+                {
+                    continue;
+                }
+                
+                mirrors.Add(new Position(x, y), line[x]); 
+            }
+        }
+        
+        foreach (var keyValuePair in mirrors)
+        {
+            Console.WriteLine(keyValuePair);    
         }
     }
 
